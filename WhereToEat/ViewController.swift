f04ec9b82df1e82d65092a0f657548b9857670eb
dagny <moonchild174@gmail.com>
2020-04-28 11:58:44 +0300

    Initial Commit

---
+++ WhereToEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhereToEat
+//
+//  Created by Daria Kalmykova on 28.04.2020.
+//  Copyright © 2020 Daria Kalmykova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
